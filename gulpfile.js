2c95a54450c4ba19d9fd3649a9133f1199bae6f3
Chandresh <chandresh989@gmail.com>
2015-07-11 21:51:37 +0530

    setup gulp and gulpfile.js and added padding on home page

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('css', function() {
+    gulp.src('app/assets/sass/main.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 10 version'))
+        .pipe(gulp.dest('public/css'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
