34df1d061c598a481d1b32f998522c35d99ccaac
Chandresh <chandresh989@gmail.com>
2015-07-11 20:38:50 +0530

    added assets folder in app for using sass and added some lines for home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Welcome to Devbook</h1>
+    <div class="jumbotron">
+        <h1>Welcome to Devbook</h1>
+            <p>A place where all of you can talk about your group work</p>
+            <p>To Start using it, Sign Up Now.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a></p>
+    </div>
 @stop
